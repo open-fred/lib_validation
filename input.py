d645f61a329eba8608218f272503debc76411903
SabineHaas <sabine.haas@rl-institut.de>
2017-10-19 17:00:14 +0200

    Function get_data() with pickle

--- input.py
+++ input.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import pickle
 #import numpy as np
 
 
@@ -57,7 +58,6 @@ def check_column_names(filename):
 #                print(column_names)
 
 
-#check_column_names('filenames.txt')
 def data_evaluation(filename):
     """
     Evaluate the data in terms of which variables are given for each dataset.
@@ -79,6 +79,11 @@ def data_evaluation(filename):
                 df_compare = pd.concat([df_compare, df2], axis=1)
     df_compare.to_csv('evaluation.csv')
     return df_compare
+
+
+def get_data(filename_files, filename_column_names, new_column_names,
+             filename_pickle='pickle_dump.p', pickle_load=True):
+    if not pickle_load:
 #        usecols = [0]
 #        with open(filename_column_names) as file:
 #            for line in file:
@@ -88,14 +93,24 @@ def data_evaluation(filename):
         with open(filename_column_names) as file:
             for line in file:
                 line = line.strip()
-    with open('filenames.txt') as file:
+                filter_cols.append(line)
+        with open(filename_files) as file:
+            data = pd.DataFrame()
             for line in file:
                 name = line.strip()
 #                df = read_data(name, usecols=usecols)
                 df = restructure_data(name, filter_cols)
+                df.columns = new_column_names
+                data = pd.concat([data, df])  # data could also be dictionary
+    pickle.dump(data, open(filename_pickle, 'wb'))
+    if pickle_load:
+        data = pickle.load(open(filename_pickle, 'rb'))
+    return data
 
+#check_column_names('filenames_2015.txt')
+#df_compare = data_evaluation('filenames_all.txt')
 
-new_names = [
+new_column_names_2016_2017 = [
     'Bredstedt_P_W', 'Bredstedt_P_W_theo', 'Bredstedt_v_wind',
     'Bredstedt_wind_dir', 'Bredstedt_P_W_inst', 'Goeser_P_W',
     'Goeser_P_W_theo', 'Goeser_v_wind', 'Goeser_wind_dir', 'Goeser_P_W_inst',
@@ -105,8 +120,23 @@ new_names = [
     'PPC_4950_P_W_inst', 'PPC_5598_P_W', 'PPC_5598_P_W_theo',
     'PPC_5598_v_wind', 'PPC_5598_wind_dir', 'PPC_5598_P_W_inst']
 
-get_dfs_and_rename_columns(new_names)
+new_column_names_2015 = [
+    'Bredstedt_P_W', 'Bredstedt_P_W_theo', 'Bredstedt_v_wind',
+    'Bredstedt_wind_dir', 'Bredstedt_P_W_inst',
+    'Nordstrand_P_W', 'Nordstrand_P_W_inst', 'Nordstrand_P_W_inst',
+    'PPC_4919_P_W', 'PPC_4919_P_W_theo', 'PPC_4950_P_W',
+    'PPC_4950_P_W_theo', 'PPC_4950_v_wind', 'PPC_5598_P_W',
+    'PPC_5598_P_W_inst', 'PPC_5598_P_W_theo']
 
+#data = get_data('filenames_2015.txt', 'column_names_2015.txt',
+#                new_column_names_2015, 'data_2015.p', pickle_load=False)
+
+
+data = get_data('filenames_2016_2017.txt', 'column_names_2016_2017.txt',
+                new_column_names_2016_2017, 'data_2016_2017.p',
+                pickle_load=False)
+data.to_csv('ergebnis.csv')
+#get_data(new_names, 'filenames_2015.txt')
 
 #df.to_csv('out_1.csv', sep='\t')
 #df2.to_csv('out.csv', sep='\t')
