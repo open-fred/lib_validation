a4e81e71a5bfa7230bacacc32843e7f68166646b
SabineHaas <sabine.haas@rl-institut.de>
2017-10-25 16:48:51 +0200

    Form main script

--- windpowerlib_validation.py
+++ windpowerlib_validation.py
@@ -1,10 +1,23 @@
-from windpowerlib import wind_turbine
+from windpowerlib import wind_turbine as wt
+from windpowerlib import wind_farm as wf
 import small_tools
-
+import tools
+import os
+import pandas as pd
+# TODO: rename to main.py
 # Get all turbine types of windpowerlib
-turbines = wind_turbine.get_turbine_types(print_out=False)
-small_tools.print_all_turbine_types(turbines)
+#turbines = wind_turbine.get_turbine_types(print_out=False)
+#small_tools.print_all_turbine_types(turbines)
 
+# ----------------------------- Set parameters ------------------------------ #
+pickle_load = True
+filename = os.path.join(os.path.dirname(__file__),
+                        'dumps/weather', 'weather_df_merra_2015.p')
+weather_data = 'merra'
+year = 2015
+
+# --------------------- Turbine data and initialization --------------------- #
+# TODO: scale power curves??
 # Turbine data
 enerconE70 = {
     'turbine_name': 'ENERCON E 70 2300',  # NOTE: Peak power should be 2.37 MW - is 2,31 for turbine in windpowerlib
@@ -17,10 +30,73 @@ enerconE66 = {
     'rotor_diameter': 70  # in m    source: www.wind-turbine-models.com
 }
 
-# TODO: scale power curves
+# Initialize WindTurbine objects
+e70 = wt.WindTurbine(**enerconE70)
+e66 = wt.WindTurbine(**enerconE66)
+#small_tools.plot_or_print(e70, e66)
 
-# Initialise WindTurbine objects
-e70 = wind_turbine.WindTurbine(**enerconE70)
-e66 = wind_turbine.WindTurbine(**enerconE66)
+# ----------------------------- Wind farm data ------------------------------ #
+# Bredstedt (54.578219, 8.978092)
+bredstedt = {
+    'wind_farm_name': 'Bredstedt',
+    'wind_turbine_fleet': [{'wind_turbine': e70,
+                            'number_of_turbines': 16}],
+    'coordinates': [55, 8.75]
+}
+# Nordstrand 
+nordstrand = {
+    'wind_farm_name': 'Nordstrand',
+    'wind_turbine_fleet': [{'wind_turbine': e70,
+                            'number_of_turbines': 6}],
+    'coordinates': [55, 8.75]
+}
+# PPC_4919
+PPC_4919 = {
+    'wind_farm_name': 'PPC_4919',
+    'wind_turbine_fleet': [{'wind_turbine': e70,
+                            'number_of_turbines': 13},
+                           {'wind_turbine': e66,
+                            'number_of_turbines': 4}],
+    'coordinates': [55, 8.75]
+}
+# PPC_4950
+PPC_4950 = {
+    'wind_farm_name': 'PPC_4950',
+    'wind_turbine_fleet': [{'wind_turbine': e70,
+                            'number_of_turbines': 22}],
+    'coordinates': [55, 8.75]
+}
+# PPC_5598
+PPC_5598 = {
+    'wind_farm_name': 'PPC_5598',
+    'wind_turbine_fleet': [{'wind_turbine': e70,
+                            'number_of_turbines': 14}],
+    'coordinates': [55, 8.75]
+}
 
-small_tools.plot_or_print(e70, e66)
+# Create data frame from csv if pickle_load == False
+if pickle_load:
+        data_frame = None
+else:
+    print('Read MERRA data from csv...')
+    data_frame = pd.read_csv(os.path.join(
+        os.path.dirname(__file__), 'data/Merra',
+        'weather_data_GER_{0}.csv'.format(year)),
+        sep=',', decimal='.', index_col=0)
+farms = []
+wind_farm_data = [bredstedt, nordstrand, PPC_4919, PPC_4950, PPC_5598]
+for description in wind_farm_data:
+    # Initialise wind farm
+    wind_farm = wf.WindFarm(**description)
+    # Get weather
+    weather = tools.get_weather_data(pickle_load, filename, weather_data,
+                                     year, wind_farm.coordinates, data_frame)
+    data_height = {'wind_speed': 50,  # Source: https://data.open-power-system-data.org/weather_data/2017-07-05/
+                   'roughness_length': 0,  # TODO: is this specified?
+                   'temperature': weather.temperature_height,
+                   'density': 0,
+                   'pressure': 0}
+    # Get power output
+    wind_farm.power_output = tools.power_output_sum(
+        wind_farm.wind_turbine_fleet, weather, data_height)
+    farms.append(wind_farm)
