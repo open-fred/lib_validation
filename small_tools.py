ec5f081bef62a3065724719d1450c66c5fa3a606
SabineHaas <sabine.haas@rl-institut.de>
2017-10-26 12:39:38 +0200

    Make plot_or_print() function more general

--- small_tools.py
+++ small_tools.py
@@ -1,8 +1,5 @@
 import pandas as pd
-try:
-    from matplotlib import pyplot as plt
-except ImportError:
-    plt = None
+from matplotlib import pyplot as plt
 
 
 def return_lats_lons(df):
@@ -18,34 +15,30 @@ def print_all_turbine_types(df):
     pd.reset_option('display.max_rows')
 
 
-def plot_or_print(turbine_1, turbine_2):
+def plot_or_print(turbine_or_farm, plot=True, print_out=False):
     r"""
     Plots or prints power output and power (coefficient) curves.
 
     """
-    if plt:
-        if turbine_2.power_coefficient_curve is not None:
-            turbine_2.power_coefficient_curve.plot(
-                x='wind_speed', y='values', style='*',
-                title=str(turbine_2.turbine_name) + ' power coefficient curve')
+    if plot:
+        if turbine_or_farm.power_coefficient_curve is not None:
+            turbine_or_farm.power_coefficient_curve.plot(
+                x='wind_speed', y='values', style='*', title=str(
+                    turbine_or_farm.turbine_name) + ' power coefficient curve')
             plt.show()
-        if turbine_2.power_curve is not None:
-            turbine_2.power_curve.plot(x='wind_speed', y='values', style='*',
-                                       title=str(turbine_2.turbine_name) +
-                                       ' power curve')
+        if turbine_or_farm.power_curve is not None:
+            turbine_or_farm.power_curve.plot(
+                x='wind_speed', y='values', style='*', title=str(
+                    turbine_or_farm.turbine_name) + ' power curve')
             plt.show()
-        if turbine_1.power_coefficient_curve is not None:
-            turbine_1.power_coefficient_curve.plot(
-                x='wind_speed', y='values', style='*',
-                title=str(turbine_1.turbine_name) + ' power coefficient curve')
+        if turbine_or_farm.power_output is not None:
+            turbine_or_farm.power_output.plot(
+                x='timestamp', y='power', style='*', title=str(
+                    turbine_or_farm.turbine_name) + ' power output')
             plt.show()
-        if turbine_1.power_curve is not None:
-            turbine_1.power_curve.plot(x='wind_speed', y='values', style='*',
-                                       title=str(turbine_1.turbine_name) +
-                                       ' power coefficient curve')
-            plt.show()
-    else:
-        if turbine_2.power_coefficient_curve is not None:
-            print(turbine_2.power_coefficient_curve)
-        if turbine_2.power_curve is not None:
-            print(turbine_2.power_curve)
+    if print_out:
+        if turbine_or_farm.power_coefficient_curve is not None:
+            print(turbine_or_farm.power_coefficient_curve)
+        if turbine_or_farm.power_curve is not None:
+            print(turbine_or_farm.power_curve)
+        if turbine_or_farm.power_output is not None:
