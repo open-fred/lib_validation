0ef040be8ea1eb4fe1e541bee8ad5a983fafaa24
Sabine <sabine.haas@rl-institut.de>
2017-11-27 17:45:18 +0100

    Adapt tests to analysis_tools module

--- test_analysis_tools.py
+++ test_analysis_tools.py
@@ -4,13 +4,38 @@ import analysis_tools
 
 class TestPearson:
     def test_pearson_s_r(self):
-        # Identical series
         series_1 = pd.Series([1., 2., 3.])
         series_2 = pd.Series([1., 2., 3.])
+        series_3 = pd.Series([3., 4., 5.])
+        series_4 = pd.Series([2., 4., 6.])
+        # Identical series
+        val_obj_1 = analysis_tools.ValidationObject('Test', series_1, series_2)
         r_exp = 1.0
-        assert r_exp == analysis_tools.pearson_s_r(series_1, series_2)
-        
-        # Correlation < 1.0
-        series_2 = pd.Series([3., 1.8, 4.])
-        r_exp = 0.45392064950160177
-        assert r_exp == analysis_tools.pearson_s_r(series_1, series_2)
+        mean_bias_exp = 0
+        std_dev_exp = 0
+        assert r_exp == val_obj_1.pearson_s_r
+        assert mean_bias_exp == val_obj_1.mean_bias
+        assert std_dev_exp == val_obj_1.standard_deviation
+
+        # Simulated series fully correlate but with bias
+        val_obj_2 = analysis_tools.ValidationObject('Test', series_1, series_3)
+        r_exp = 1.0
+        mean_bias_exp = 2.0
+        std_dev_exp = 0
+        assert r_exp == val_obj_2.pearson_s_r
+        assert mean_bias_exp == val_obj_2.mean_bias
+        assert std_dev_exp == val_obj_2.standard_deviation
+
+        # Simulated series = 2 * validation series
+        val_obj_3 = analysis_tools.ValidationObject('Test', series_1, series_4)
+        r_exp = 1.0
+        mean_bias_exp = 2.0
+        std_dev_exp = 0.81649658092772603
+        assert r_exp == val_obj_3.pearson_s_r
+        assert mean_bias_exp == val_obj_3.mean_bias
+        assert std_dev_exp == val_obj_3.standard_deviation
+
+#        # Correlation < 1.0
+#        series_2 = pd.Series([3., 1.8, 4.])
+#        r_exp = 0.45392064950160177
+#        assert r_exp == analysis_tools.pearson_s_r(series_1, series_2)
