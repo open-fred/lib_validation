95a6e5aa4843fe68c8fa7941036afe855c0453ba
Sabine <sabine.haas@rl-institut.de>
2017-11-27 18:02:13 +0100

    Add to tests

--- test_analysis_tools.py
+++ test_analysis_tools.py
@@ -8,6 +8,7 @@ class TestPearson:
         series_2 = pd.Series([1., 2., 3.])
         series_3 = pd.Series([3., 4., 5.])
         series_4 = pd.Series([2., 4., 6.])
+        series_5 = pd.Series([2., 4., 5.])
         # Identical series
         val_obj_1 = analysis_tools.ValidationObject('Test', series_1, series_2)
         r_exp = 1.0
@@ -34,6 +35,16 @@ class TestPearson:
         assert r_exp == val_obj_3.pearson_s_r
         assert mean_bias_exp == val_obj_3.mean_bias
         assert std_dev_exp == val_obj_3.standard_deviation
+
+#        # Not linear
+#        val_obj_4 = analysis_tools.ValidationObject('Test', series_1, series_5)
+#        r_exp = 1.0
+#        mean_bias_exp = 2.0
+#        std_dev_exp = 0.81649658092772603
+#        print(val_obj_4.get_standard_deviation_from_zero(val_obj_4.bias))
+#        assert r_exp == val_obj_4.pearson_s_r
+#        assert mean_bias_exp == val_obj_4.mean_bias
+#        assert std_dev_exp == val_obj_4.standard_deviation
 
 #        # Correlation < 1.0
 #        series_2 = pd.Series([3., 1.8, 4.])
