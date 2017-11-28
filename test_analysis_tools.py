8cc93624630b93e89736dfbcda67eab8d5badc36
SabineHaas <sabine.haas@rl-institut.de>
2017-11-28 10:58:55 +0100

    Add test for standard deviation

--- test_analysis_tools.py
+++ test_analysis_tools.py
@@ -3,14 +3,20 @@ import analysis_tools
 
 
 class TestPearson:
-    def test_pearson_s_r(self):
-        # Identical series
-        series_1 = pd.Series([1., 2., 3.])
-        series_2 = pd.Series([1., 2., 3.])
-        r_exp = 1.0
-        assert r_exp == analysis_tools.pearson_s_r(series_1, series_2)
-        
-        # Correlation < 1.0
-        series_2 = pd.Series([3., 1.8, 4.])
-        r_exp = 0.45392064950160177
-        assert r_exp == analysis_tools.pearson_s_r(series_1, series_2)
+#    def test_get_pearson_s_r(self):
+#        # Identical series
+#        series_1 = pd.Series([1., 2., 3.])
+#        series_2 = pd.Series([1., 2., 3.])
+#        r_exp = 1.0
+#        assert r_exp == analysis_tools.get_pearson_s_r(series_1, series_2)
+#        
+#        # Correlation < 1.0
+#        series_2 = pd.Series([3., 1.8, 4.])
+#        r_exp = 0.45392064950160177
+#        assert r_exp == analysis_tools.get_pearson_s_r(series_1, series_2)
+
+
+    def test_get_standard_deviation(self):
+        series = pd.Series([1, 2, 3, 4])
+        dev_exp = 1.1180339887498949
+        assert dev_exp == analysis_tools.get_standard_deviation(series)
