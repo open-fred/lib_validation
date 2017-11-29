7be1a2cc35c143c473a87dd3a457446965603950
birgit_s <birgit.schachler@rl-institut.de>
2017-11-29 15:02:07 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='lib_validation',
+      version='0.0.1',
+      description='Scripts to validate the pvlib and windpowerlib.',
+      url='http://github.com/open-fred/lib_validation',
+      author='oemof developing group',
+      author_email='',
+      license='GPL3',
+      packages=[],
+      zip_safe=False,
+      install_requires=['pandas',
+                        'matplotlib',
+                        'pvlib >= 0.5.0',
+                        'windpowerlib >= 0.0.6'])
