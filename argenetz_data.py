44cfa69a13bbc604f0151e9a9baf929ea2fd5f2b
Sabine <sabine.haas@rl-institut.de>
2018-01-16 15:41:27 +0100

    Add missing column name

--- argenetz_data.py
+++ argenetz_data.py
@@ -203,10 +203,10 @@ def plot_argenetz_data(df, save_folder, y_limit=None, x_limit=None):
     plt.close()
 
 #new_column_names_2016_2017 = [
-#    'wf_1_power_output', 'wf_1_installed_power', 'wf_3_power_output',
-#    'wf_3_wind_speed', 'wf_3_installed_power', 'wf_4_power_output',
-#    'wf_4_wind_speed', 'wf_4_installed_power', 'wf_5_power_output',
-#    'wf_5_wind_speed', 'wf_5_installed_power']
+#    'wf_1_power_output', 'wf_1_wind_speed', 'wf_1_installed_power',
+#    'wf_3_power_output', 'wf_3_wind_speed', 'wf_3_installed_power',
+#    'wf_4_power_output', 'wf_4_wind_speed', 'wf_4_installed_power',
+#    'wf_5_power_output', 'wf_5_wind_speed', 'wf_5_installed_power']
 #
 #new_column_names_2015 = [
 #    'wf_1_power_output', 'wf_1_wind_speed', 'wf_1_installed_power',
