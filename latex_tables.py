24db7b200a276ba7e51f412cd818ce4236427de1
SabineHaas <sabine.haas@rl-institut.de>
2018-02-28 14:22:29 +0100

    Delete unused functions

--- latex_tables.py
+++ latex_tables.py
@@ -20,67 +20,6 @@ def create_column_format(number_of_columns, position='c', index_columns='l'):
         return column_format
 
 
-def get_columns(column_names, multiplikator):
-    r"""
-    Produces columns for pd.DataFrame needed for latex output.
-
-    Parameters
-    ----------
-    column_names : List
-        Contains column names (String).
-    multiplikator : Integer
-        Frequency of column names.
-
-    Returns
-    -------
-    columns : List
-        Column names for pd.DataFrame needed for latex output.
-
-    """
-    columns = []
-    for column_name in column_names:
-        columns.extend([column_name for i in range(multiplikator)])
-    return columns
-
-
-def get_data(validation_sets, data_names, object_position, length):
-    r"""
-    Retruns list containing data for pd.DataFrame needed for latex output.
-
-    Paramters
-    ---------
-    validation_sets : List
-        Contains lists of :class:`~.analysis_tools.ValidationObject` objects.
-    data_names : List
-        Contains specification of data (Strings) to be displayed.
-    object_position : Integer
-        Position of object in lists in `validation_sets`.
-    length : Integer
-        Lenth of TODO: add # might be changed
-
-    Returns
-    -------
-    data : List
-        Data for pd.DataFrame needed for latex output.
-
-    """
-    data = []
-    if 'RMSE' in data_names:
-        data.extend([round(validation_sets[j][object_position].rmse, 2)
-                     for j in range(length)])
-    if 'Pr' in data_names:
-        data.extend([round(validation_sets[j][object_position].pearson_s_r, 2)
-                     for j in range(length)])
-    if 'mean bias' in data_names:
-        data.extend([round(validation_sets[j][object_position].mean_bias, 2)
-                     for j in range(length)])
-    if 'std. dev.' in data_names:
-        data.extend([round(
-            validation_sets[j][object_position].standard_deviation, 2)
-            for j in range(length)])
-    return data
-
-
 def write_latex_output(latex_output, weather_data_list, approach_list,
                        restriction_list, val_obj_dict, annual_energy_dicts,
                        wind_farm_names, key_figures_print, output_methods,
