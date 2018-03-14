906065c807705ddb597e7ef01a557eaf47bebb4c
SabineHaas <sabine.haas@rl-institut.de>
2018-03-14 15:35:56 +0100

    Evaluation of error numbers

--- greenwind_data.py
+++ greenwind_data.py
@@ -50,7 +50,7 @@ def read_data(filename):
 
 def get_greenwind_data(year, pickle_load=False, filename='greenwind_dump.p',
                       resample=True, plot=False, x_limit=None,
-                      frequency='30T'):
+                      frequency='30T', pickle_dump=True):
     # TODO: add plots to check data
     r"""
     Fetches GreenWind data.
@@ -77,6 +77,8 @@ def get_greenwind_data(year, pickle_load=False, filename='greenwind_dump.p',
         wanted. Default: None.
     frequency : String (or freq object...?)
         # TODO add
+    pickle_dump : Boolean
+        If True the data frame is dumped to `filename`. Default: True.
 
     Returns
     -------
@@ -88,14 +90,14 @@ def get_greenwind_data(year, pickle_load=False, filename='greenwind_dump.p',
         greenwind_df = pickle.load(open(filename, 'rb'))
     else:
         filenames = [
-            'WF1_{0}.csv'.format(year),
+            # 'WF1_{0}.csv'.format(year),
             'WF2_{0}.csv'.format(year),
             'WF3_{0}.csv'.format(year)]
         greenwind_df = pd.DataFrame()
         for name in filenames:
-            df_part = read_data(name)
+            df_part = read_data(name).drop_duplicates()
             # Add to DataFrame
-            greenwind_df = pd.concat([greenwind_df, df_part], axis=0)
+            greenwind_df = pd.concat([greenwind_df, df_part], axis=1)
         # Convert index to DatetimeIndex and make time zone aware
         greenwind_df.index = pd.to_datetime(greenwind_df.index).tz_localize(
             'UTC').tz_convert('Europe/Berlin')
@@ -105,8 +107,26 @@ def get_greenwind_data(year, pickle_load=False, filename='greenwind_dump.p',
             # Add frequency attribute
             freq = pd.infer_freq(greenwind_df.index)
             greenwind_df.index.freq = pd.tseries.frequencies.to_offset(freq)
-        pickle.dump(greenwind_df, open(filename, 'wb'))
+        if pickle_dump:
+            pickle.dump(greenwind_df, open(filename, 'wb'))
     return greenwind_df
+
+
+def get_error_numbers(year):
+    filename = os.path.join(
+        os.path.dirname(__file__), 'dumps/validation_data',
+        'greenwind_data_{0}.p'.format(year))
+    df = get_greenwind_data(year=year, resample=False,
+                            pickle_load=False, filename=filename,
+                            pickle_dump=False)
+    error_numbers = []
+    for column_name in list(df):
+        if 'error_number' in column_name:
+            error_numbers.extend(df[column_name].unique())
+    sorted_error_numbers = pd.Series(
+        pd.Series(error_numbers).unique()).sort_values()
+    sorted_error_numbers.index = np.arange(len(sorted_error_numbers))
+    return sorted_error_numbers
 
 
 if __name__ == "__main__":
@@ -114,10 +134,27 @@ if __name__ == "__main__":
         2015,
         2016
     ]
-    # Decide whether to resample to a certain frequency
+    Decide whether to resample to a certain frequency
     resample = True
     frequency = '30T'
     for year in years:
         filename = os.path.join(os.path.dirname(__file__), 'dumps/validation_data',
                                 'greenwind_data_{0}.p'.format(year))
         df = get_greenwind_data(year=year, resample=resample, filename=filename)
+
+    # Evaluation of error numbers - decide whether to execute:
+    error_numbers = False
+    if error_numbers:
+        error_numbers_total =[]
+        for year in years:
+            error_numbers = get_error_numbers(year)
+            error_numbers.to_csv(
+                '../../../User-Shares/Masterarbeit/Daten/Twele/' +
+                'error_numbers_{}.csv'.format(year))
+            error_numbers_total.extend(error_numbers)
+        sorted_error_numbers_total = pd.Series(
+            pd.Series(error_numbers_total ).unique()).sort_values()
+        sorted_error_numbers_total.index = np.arange(
+            len(sorted_error_numbers_total ))
+        sorted_error_numbers_total.to_csv(
+            '../../../User-Shares/Masterarbeit/Daten/Twele/error_numbers_total.csv')
