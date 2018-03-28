a36b8baaefef133dca20cb85ec1c707bdf2dd463
birgit_s <birgit.schachler@rl-institut.de>
2018-03-28 13:31:39 +0200

    Working commit

--- pvlib_validation.py
+++ pvlib_validation.py
@@ -5,8 +5,7 @@ import os
 from collections import OrderedDict
 
 import pvlib
-from pvlib.pvsystem import PVSystem
-from pvlib.location import Location
+
 from pvlib.modelchain import ModelChain
 from pvlib import irradiance
 
@@ -16,187 +15,25 @@ import analysis_tools
 import tools
 
 
-#ToDo nochmal prüfen welche Werte Instantanwerte und Mittelwerte sind und am
-# besten Index bei Mittelwerten auf Mittelpunkt des Zeitintervalls setzen;
-# resample der HTW Werte eventuell nochmal anpassen
-
-def setup_correlation_df(htw_weather_data_df, reanalysis_weather_data_df,
-                         parameter, weather_data='open_FRED', corrected=True):
-    # Note: in case of DNI FRED DNI and calculated DNI (calculated from FRED
-    # GHI and DHI) are compared; in case of GHI and GNI HTW (measured) values
-    # are compared to FRED (calculated) values
-
-    # setup dataframe with given parameter of HTW and FRED weather dataset
-    if parameter == 'dni':
-        df = calculate_dni_pvlib(reanalysis_weather_data_df, weather_data,
-                                 corrected)
-    else:
-        df = reanalysis_weather_data_df[parameter].to_frame().join(
-            htw_weather_data_df[parameter].to_frame(),
-            lsuffix='_fred', rsuffix='_htw')
-    return df
-
-
-def compare_parameters(df, parameter, resample_rule, plot_directory):
-    # calculate correlation
-    corr = analysis_tools.correlation_tmp(df, resample_rule=resample_rule)
-
-    # plot correlation
-    corr.plot()
-    mpl.savefig(os.path.join(
-        plot_directory, '{}_correlation_{}.png'.format(
-            parameter, resample_rule)))
-
-    # calculate RMSE
-    rmse = analysis_tools.variability(df, resample_rule=resample_rule)
-
-    # plot correlation
-    rmse.plot()
-    mpl.savefig(os.path.join(
-        plot_directory, '{}_rmse_{}.png'.format(
-            parameter, resample_rule)))
-
-
-def plot_week(df, parameter, weather_data, measured_data, plot_directory,
-              winter_week=('1/25/2015', '2/1/2015'),
-              summer_week=('6/2/2015', '6/8/2015')):
-    # winter_week - tuple with start and end date
-
-    # plot winter week
-
-    # set frequency of index
-    if weather_data == 'open_FRED':
-        index = pd.date_range(start=winter_week[0], end=winter_week[1],
-                              freq='30Min', tz='UTC') \
-                - pd.Timedelta(minutes=15)
-    elif weather_data == 'MERRA':
-        index = pd.date_range(start=winter_week[0], end=winter_week[1],
-                              freq='60Min', tz='UTC')
-
-    df.loc[index, :].plot()
-    mpl.savefig(os.path.join(plot_directory, '{}_winter_week_{}_{}.png'.format(
-        parameter, weather_data, measured_data)))
-
-    # plot summer week
-
-    # set frequency of index
-    if weather_data == 'open_FRED':
-        index = pd.date_range(start=summer_week[0], end=summer_week[1],
-                              freq='30Min', tz='UTC') \
-                - pd.Timedelta(minutes=15)
-    elif weather_data == 'MERRA':
-        index = pd.date_range(start=summer_week[0], end=summer_week[1],
-                              freq='60Min', tz='UTC')
-
-    df.loc[index, :].plot()
-    mpl.savefig(os.path.join(plot_directory, '{}_summer_week_{}_{}.png'.format(
-        parameter, weather_data, measured_data)))
-
-
-def calculate_dni_pvlib(weather_df, weather_data, corrected=True):
-
-    if weather_data == 'open_FRED':
-        # save initial index in column 'time'
-        weather_df_copy = weather_df.copy()
-        weather_df_copy.reset_index(inplace=True)
-        weather_df_copy.set_index('time', drop=False, inplace=True)
-        weather_df_copy['new_index'] = weather_df_copy.index + \
-                                       pd.Timedelta(minutes=15)
-        weather_df_copy.set_index('new_index', drop=True, inplace=True)
-
-    # calculate DNI
-    times = weather_df_copy.index
-    location = setup_pvlib_location_object()
-    solarposition = location.get_solarposition(
-        times, pressure=None, temperature=weather_df_copy['temp_air'])
-    if corrected:
-        # calculate corrected DNI
-        clearsky = location.get_clearsky(times, solar_position=solarposition)
-        dni = irradiance.dni(weather_df_copy['ghi'], weather_df_copy['dhi'],
-                             zenith=solarposition['zenith'],
-                             clearsky_dni=clearsky['dni'],
-                             clearsky_tolerance=1.1,
-                             zenith_threshold_for_zero_dni=88.0,
-                             zenith_threshold_for_clearsky_limit=80.0)
-    else:
-        dni = (weather_df_copy['ghi'] - weather_df_copy['dhi']) / np.cos(
-            np.radians(solarposition['zenith']))
-
-    # setup df with calculated and Fred DNI
-    dni.name = 'dni'
-    df = weather_df_copy['dni'].to_frame().join(
-        dni.to_frame(),
-        lsuffix='_fred', rsuffix='_pvlib')
-    dni.set_index('time', inplace=True)
-
-    return df
-
-
-def setup_pvlib_location_object():
-    return Location(latitude=52.456032, longitude=13.525282,
-                    tz='UTC', altitude=60, name='HTW Berlin')
-
-
-def setup_htw_pvlib_pvsystem(converter_number):
-
-    # get module and inverter parameters
-    sandia_modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
-    sandia_inverters = pvlib.pvsystem.retrieve_sam('sandiainverter')
-    CEC_modules = pvlib.pvsystem.retrieve_sam('CECMod')
-    CEC_inverters = pvlib.pvsystem.retrieve_sam('sandiainverter')
-
-    inv_sma = 'SMA_Solar_Technology_AG__SB3000HFUS_30___240V_240V__CEC_2011_'
-    inv_danfoss = 'Danfoss_Solar__DLX_2_9_UL__240V__240V__CEC_2013_'
-
-    # module 1 - Schott aSi 105W / Danfoss DLX 2.9
-    if converter_number == 'wr1':
-        pass
-    # module 2 - Aleo S19 285W / Danfoss DLX 2.9 'Aleo_Solar_S19H270' CEC
-    elif converter_number == 'wr2':
-        pass
-
-    # module 3 - Aleo S18 240W / Danfoss DLX 2.9
-    elif converter_number == 'wr3':
-        pv_module = PVSystem(module='aleo_solar_S18_240', inverter=inv_danfoss,
-                             module_parameters=CEC_modules[
-                                 'aleo_solar_S18_240'],
-                             inverter_parameters=CEC_inverters[inv_danfoss],
-                             surface_tilt=14.57, surface_azimuth=215.,
-                             albedo=0.2,
-                             modules_per_string=14, strings_per_inverter=1,
-                             name='HTW_module_3')
-        pv_module.module_parameters['EgRef'] = 1.121
-        pv_module.module_parameters['dEgdT'] = -0.0002677
-        pv_module.module_parameters['alpha_sc'] = 0.04
-
-    # module 4 - Aleo S19 245W / SMA SB 3000HF-30
-    elif converter_number == 'wr4':
-        pv_module = PVSystem(module='Aleo_Solar_S19U245_ulr', inverter=inv_sma,
-                             module_parameters=CEC_modules[
-                                 'Aleo_Solar_S19U245_ulr'],
-                             inverter_parameters=CEC_inverters[inv_sma],
-                             surface_tilt=14.57, surface_azimuth=215.,
-                             albedo=0.2,
-                             modules_per_string=13, strings_per_inverter=1,
-                             name='HTW_module_4')
-        pv_module.module_parameters['EgRef'] = 1.121
-        pv_module.module_parameters['dEgdT'] = -0.0002677
-        pv_module.module_parameters['alpha_sc'] = 0.03
-    # module 5 - Schott aSi 105W / SMA SB 3000HF-30
-    elif converter_number == 'wr5':
-        pass
-    return pv_module
-
-
-def setup_and_run_modelchain(pv_system, location, weather_data):
-
-    mc = ModelChain(system=pv_system, location=location,
-                    aoi_model='no_loss', spectral_model='no_loss')
-    mc.run_model(weather_data.index, weather=weather_data)
-    return mc
-
-
 def reindl(ghi, i0_h, elevation):
+    """
+    Reindl model to calculate DNI and DHI from GHI and I0_h.
+
+    Parameters
+    -----------
+    ghi : :pandas:`DataFrame` or :numpy:`array`
+        Time series for global horizontal irradiance in W/m².
+    i0_h : :pandas:`DataFrame` or :numpy:`array`
+        Time series for irradiance on top of atmosphere in W/m².
+    elevation : :obj:`float`
+        Elevation of location in m.
+
+    Returns
+    -------
+    :collections:`OrderedDict`
+        Dictionary with time series for DNI, DHI and clearness index kt.
+
+    """
 
     elevation = pvlib.tools.sind(elevation)
 
@@ -233,43 +70,64 @@ def reindl(ghi, i0_h, elevation):
     return data
 
 
-def decomposition(merra_df, model, location):
+def apply_decomposition_model(weather_df, model, location):
+    """
+    Uses the specified decomposition model to calculate DNI and DHI.
+
+    Parameters
+    ----------
+    weather_df : :pandas:`DataFrame`
+         Weather DataFrame containing all variables needed to apply the
+         decomposition model. See model functions for more information.
+    model : :obj:`str`
+        Decomposition model to use. Choose from 'reindl', 'erbs' or 'disc'.
+    location : :pvlib:`Location`
+
+    Returns
+    -------
+    :pandas:`DataFrame`
+     DataFrame with DNI and DHI.
+
+    """
 
     solar_position = location.get_solarposition(
-        merra_df.index, pressure=merra_df['pressure'].mean(),
-        temperature=merra_df['temp_air'].mean())
+        weather_df.index, pressure=weather_df['pressure'].mean(),
+        temperature=weather_df['temp_air'].mean())
 
     if model == 'reindl':
 
         solar_position = location.get_solarposition(
-            merra_df.index, pressure=merra_df['pressure'].mean(),
-            temperature=merra_df['temp_air'].mean())
+            weather_df.index, pressure=weather_df['pressure'].mean(),
+            temperature=weather_df['temp_air'].mean())
 
-        df = reindl(merra_df.ghi, merra_df.i0_h, solar_position.elevation)
+        df = reindl(weather_df.ghi, weather_df.i0_h, solar_position.elevation)
         df['dni_corrected'] = irradiance.dni(
-            merra_df['ghi'], df['dhi'], solar_position.zenith,
+            weather_df['ghi'], df['dhi'], solar_position.zenith,
             clearsky_dni=location.get_clearsky(
-                merra_df.index, solar_position=solar_position).dni,
+                weather_df.index, solar_position=solar_position).dni,
             clearsky_tolerance=1.1,
             zenith_threshold_for_zero_dni=88.0,
             zenith_threshold_for_clearsky_limit=80.0)
 
     elif model == 'erbs':
 
-        df = irradiance.erbs(merra_df.ghi, solar_position.zenith,
-                                  merra_df.index)
+        df = irradiance.erbs(weather_df.ghi, solar_position.zenith,
+                             weather_df.index)
         df['dni_corrected'] = irradiance.dni(
-            merra_df['ghi'], df['dhi'], solar_position.zenith,
+            weather_df['ghi'], df['dhi'], solar_position.zenith,
             clearsky_dni=location.get_clearsky(
-                merra_df.index, solar_position=solar_position).dni,
+                weather_df.index, solar_position=solar_position).dni,
             clearsky_tolerance=1.1,
             zenith_threshold_for_zero_dni=88.0,
             zenith_threshold_for_clearsky_limit=80.0)
 
     elif model == 'disc':
 
-        df = irradiance.disc(merra_df.ghi, solar_position.zenith,
-                             merra_df.index, merra_df.pressure.mean())
+        df = irradiance.disc(weather_df.ghi, solar_position.zenith,
+                             weather_df.index, weather_df.pressure.mean())
+        df['dhi'] = weather_df.ghi - df.dni * pvlib.tools.cosd(
+            solar_position.zenith)
+        df['gni'] = df.dni + df.dhi
 
     return df
 
