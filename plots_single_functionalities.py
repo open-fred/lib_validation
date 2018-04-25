8d6cdf5578bd301c4f9441eb82e6ac2c964a16d2
Sabine <sabine.haas@rl-institut.de>
2018-04-25 11:55:40 +0200

    adapt

--- plots_single_functionalities.py
+++ plots_single_functionalities.py
@@ -156,12 +156,10 @@ def run_bar_plot_key_figures():
         # ['weather_wind_speed_1'],  # For best function for MERRA
         ['smoothing_2'],
         ['single_turbine_1'],
-        ['wake_losses_1'],
         ['wake_losses_3'],
         ['wind_farm_1']
     ]
-    not_for_monthly_list = ['wake_losses_1', 'wake_losses_2', 'wake_losses_3',
-                            'wind_farm_2', 'power_output_1',
+    not_for_monthly_list = ['wind_farm_2', 'power_output_1',
                             'single_turbine_1']
     years = [
         2015,
